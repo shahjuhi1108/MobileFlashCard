59424d6412d10d05f408adc50c10146db28a9715
Juhi <shahjuhi1108@gmail.com>
2020-05-13 16:27:24 -0700

    Initial commit

---
+++ components/Decks.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+
+class AddDeck extends Component {
+    render() {
+        return (
+            <View>
+                <Text>Decks</Text>
+            </View>
+        )
+    }
+}
+
+export default AddDeck
